d814fb2fd5e3ae1f72abb4603351b7e110083848
Miller Palacios <millerpalacios18@hotmail.com>
2021-09-29 12:40:17 -0500

    main

---
+++ js/menu.js
@@ -0,0 +1,75 @@
+const datosMenu1 = [
+  {
+    id: 1,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 9,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 11,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+  {
+    id: 12,
+    nombre: "Patata",
+    precio: 1,
+    imagen: "patata.jpg",
+  },
+];
+
