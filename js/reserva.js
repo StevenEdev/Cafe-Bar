03bef4b89079ff7f9e4954f30646f0c9145ca919
josegcort <josegcort@gmail.com>
2021-10-01 11:54:26 -0500

    Ajustes validacion reserva

--- js/reserva.js
+++ js/reserva.js
@@ -1,21 +1,16 @@
-$(document).ready(function () {
-  $("#datepicker").datepicker();
+$(document).ready(function() {
+    $("#datepicker").datepicker();
 
+    $("#formulario").on("submit", function(event) {
+        event.preventDefault();
 
-  $("#formulario").on("submit", function(event){
-    event.preventDefault();
-
-    localStorage.setItem("nombre", $("nombre").val());
-    localStorage.setItem("email", $("email").val());
-    localStorage.setItem("documento", $("documento").val());
-    localStorage.setItem("telefono", $("telefono").val());
-    localStorage.setItem("servicio", $("servicio").val());
-    localStorage.setItem("personas", $("personas").val());
-    localStorage.setItem("fecha", $("fecha").val());
-    localStorage.setItem("mensaje", $("mensaje").val());
-  });
-
-
+        localStorage.setItem("nombre", $("nombre").val());
+        localStorage.setItem("email", $("email").val());
+        localStorage.setItem("documento", $("documento").val());
+        localStorage.setItem("telefono", $("telefono").val());
+        localStorage.setItem("servicio", $("servicio").val());
+        localStorage.setItem("personas", $("personas").val());
+        localStorage.setItem("fecha", $("fecha").val());
+        localStorage.setItem("mensaje", $("mensaje").val());
+    });
 });
-
-
