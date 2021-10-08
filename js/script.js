1a7347d65c2af931288a12b3c279020b533d6b6b
josegcort <josegcort@gmail.com>
2021-10-08 12:04:04 -0500

    Elementos de la compra en el correo

--- js/script.js
+++ js/script.js
@@ -7,35 +7,34 @@ eventListeners();
 
 // all event listeners
 function eventListeners() {
-  document.addEventListener("DomContentLoaded", leerLocalStorageCompra());
+    document.addEventListener("DomContentLoaded", leerLocalStorageCompra());
 
-  document.addEventListener("change", (e) => {
-    obtenerEvento(e);
-  });
-  document.addEventListener("keyup", (e) => {
-    obtenerEvento(e);
-  });
+    document.addEventListener("change", (e) => {
+        obtenerEvento(e);
+    });
+    document.addEventListener("keyup", (e) => {
+        obtenerEvento(e);
+    });
 
-  calcularTotal();
+    calcularTotal();
 }
 
 function comprar() {
-  location.href = "compra.html";
+    location.href = "compra.html";
 }
 
 function getProductFromStorage() {
-  return localStorage.getItem("menu")
-    ? JSON.parse(localStorage.getItem("menu"))
-    : [];
-  // returns empty array if there isn't any product info
+    return localStorage.getItem("menu") ?
+        JSON.parse(localStorage.getItem("menu")) : [];
+    // returns empty array if there isn't any product info
 }
 
 function leerLocalStorageCompra() {
-  let productosLS;
-  productosLS = this.getProductFromStorage();
-  productosLS.forEach(function (item) {
-    const row = document.createElement("tr");
-    row.innerHTML = `
+    let productosLS;
+    productosLS = this.getProductFromStorage();
+    productosLS.forEach(function(item) {
+        const row = document.createElement("tr");
+        row.innerHTML = `
           <td>
               <img src="${item.imgSrc}" width=100>
           </td>
@@ -49,82 +48,83 @@ function leerLocalStorageCompra() {
           </td>
           <td id='subtotales'>${item.precio * item.cantidad}</td>
           <td>
-              <a href="#" id="basura${item.id}" onClick='eliminarCarrito(${
-      item.id
-    })' class="borrar-producto" style="font-size:30px" data-id="${
-      item.id
-    }"><i class="bi bi-trash"></i></a>
+              <a href="#" id="basura${item.id}"
+              onClick='eliminarCarrito(${item.id})' 
+              class="borrar-producto" style="font-size:30px"
+              data-id="${item.id}">
+                <i class="bi bi-trash"></i>
+              </a>
           </td>
       `;
-    listaCompra.appendChild(row);
-  });
+        listaCompra.appendChild(row);
+    });
 }
 
 function eliminarCarrito(elemento) {
-  let borrarElemento = "basura" + elemento;
-  //alert(elemento);
-  let listastorage = JSON.parse(localStorage.getItem("menu"));
-  //console.log(listastorage)
-  //console.log("---------------------------------/----------------------") 
-  var index = listastorage.findIndex(el=> el.id == elemento);
-  //console.log(index);
-  listastorage.splice(index,1)
-  let listastorageJSON = JSON.stringify(listastorage);
-  localStorage.setItem("menu",listastorageJSON) 
-  
-  
-  $(document).on('click', '.borrarl', function (event) {
-    event.preventDefault();
-    $(this).closest('tr').remove();
-  });
-  
-  location.reload()
+    let borrarElemento = "basura" + elemento;
+    //alert(elemento);
+    let listastorage = JSON.parse(localStorage.getItem("menu"));
+    //console.log(listastorage)
+    //console.log("---------------------------------/----------------------") 
+    var index = listastorage.findIndex(el => el.id == elemento);
+    //console.log(index);
+    listastorage.splice(index, 1)
+    let listastorageJSON = JSON.stringify(listastorage);
+    localStorage.setItem("menu", listastorageJSON)
+
+
+    $(document).on('click', '.borrarl', function(event) {
+        event.preventDefault();
+        $(this).closest('tr').remove();
+    });
+
+    location.reload()
 }
 
 //Calcular montos
 function calcularTotal() {
-  let productosLS;
-  let total = 0,
-    igv = 0,
-    subtotal = 0;
-  productosLS = this.getProductFromStorage();
-  for (let i = 0; i < productosLS.length; i++) {
-    let element = Number(productosLS[i].precio * productosLS[i].cantidad);
-    total = total + element;
-    console.log(total);
-  }
+    let productosLS;
+    let total = 0,
+        igv = 0,
+        subtotal = 0;
+    productosLS = this.getProductFromStorage();
+    for (let i = 0; i < productosLS.length; i++) {
+        let element = Number(productosLS[i].precio * productosLS[i].cantidad);
+        total = total + element;
+        console.log(total);
+    }
 
-  igv = parseFloat(total * 0.1).toFixed(2);
-  subtotal = parseFloat(total - igv).toFixed(2);
+    igv = parseFloat(total * 0.1).toFixed(2);
+    subtotal = parseFloat(total - igv).toFixed(2);
 
-  document.getElementById("subtotal").innerHTML = "S/. " + subtotal;
-  document.getElementById("igv").innerHTML = "S/. " + igv;
-  document.getElementById("total").value = "S/. " + total.toFixed(2);
-  let valorfinal = localStorage.setItem(
-    "ValorF",
-    document.getElementById("total").value
-  );
+    document.getElementById("subtotal").innerHTML = "S/. " + subtotal;
+    document.getElementById("igv").innerHTML = "S/. " + igv;
+    document.getElementById("total").value = "S/. " + total.toFixed(2);
+    let valorfinal = localStorage.setItem(
+        "ValorF",
+        document.getElementById("total").value
+    );
 }
 
 function obtenerEvento(e) {
-  e.preventDefault();
-  let id, cantidad, producto, productosLS;
-  if (e.target.classList.contains("cantidad")) {
-    producto = e.target.parentElement.parentElement;
-    id = producto.querySelector("a").getAttribute("data-id");
-    cantidad = producto.querySelector("input").value;
-    let actualizarMontos = document.querySelectorAll("#subtotales");
-    productosLS = this.getProductFromStorage();
-    productosLS.forEach(function (productoLS, index) {
-      if (productoLS.id === id) {
-        productoLS.cantidad = cantidad;
-        actualizarMontos[index].innerHTML = Number(
-          cantidad * productosLS[index].precio
-        );
-      }
-    });
-    localStorage.setItem("menu", JSON.stringify(productosLS));
-  } else {
-    console.log("click afuera");
-  }
+    e.preventDefault();
+    let id, cantidad, producto, productosLS;
+    if (e.target.classList.contains("cantidad")) {
+        producto = e.target.parentElement.parentElement;
+        id = producto.querySelector("a").getAttribute("data-id");
+        cantidad = producto.querySelector("input").value;
+        let actualizarMontos = document.querySelectorAll("#subtotales");
+        productosLS = this.getProductFromStorage();
+        productosLS.forEach(function(productoLS, index) {
+            if (productoLS.id === id) {
+                productoLS.cantidad = cantidad;
+                actualizarMontos[index].innerHTML = Number(
+                    cantidad * productosLS[index].precio
+                );
+            }
+        });
+        localStorage.setItem("menu", JSON.stringify(productosLS));
+    } else {
+        console.log("click afuera");
+    }
 }
